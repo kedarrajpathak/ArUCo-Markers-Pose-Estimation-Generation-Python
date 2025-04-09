0767571356acdcca1804e42d03d101ff9c187be0
Kedar <kedar.rajpathak@rwth-aachen.de>
2025-04-09 15:00:46 +0000

    calibration done, pose estimation working on webcam video feed

--- pose_estimation.py
+++ pose_estimation.py
@@ -12,7 +12,7 @@ import argparse
 import time
 
 
-def pose_esitmation(frame, aruco_dict_type, matrix_coefficients, distortion_coefficients):
+def pose_esitmation(frame, aruco_dict_type, marker_length, matrix_coefficients, distortion_coefficients):
 
     '''
     frame - Frame from the video stream
@@ -24,25 +24,26 @@ def pose_esitmation(frame, aruco_dict_type, matrix_coefficients, distortion_coef
     '''
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.aruco_dict = cv2.aruco.Dictionary_get(aruco_dict_type)
-    parameters = cv2.aruco.DetectorParameters_create()
+    cv2.aruco_dict = cv2.aruco.getPredefinedDictionary(aruco_dict_type)
+    parameters = cv2.aruco.DetectorParameters()
 
 
-    corners, ids, rejected_img_points = cv2.aruco.detectMarkers(gray, cv2.aruco_dict,parameters=parameters,
-        cameraMatrix=matrix_coefficients,
-        distCoeff=distortion_coefficients)
+    # corners, ids, rejected_img_points = cv2.aruco.detectMarkers(gray, cv2.aruco_dict,parameters=parameters,
+    #     cameraMatrix=matrix_coefficients,
+    #     distCoeff=distortion_coefficients)
+    corners, ids, rejected_img_points = cv2.aruco.detectMarkers(gray, cv2.aruco_dict,parameters=parameters)
 
         # If markers are detected
     if len(corners) > 0:
         for i in range(0, len(ids)):
             # Estimate pose of each marker and return the values rvec and tvec---(different from those of camera coefficients)
-            rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners[i], 0.02, matrix_coefficients,
+            rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners[i], marker_length, matrix_coefficients,
                                                                        distortion_coefficients)
             # Draw a square around the markers
             cv2.aruco.drawDetectedMarkers(frame, corners) 
 
             # Draw Axis
-            cv2.aruco.drawAxis(frame, matrix_coefficients, distortion_coefficients, rvec, tvec, 0.01)  
+            cv2.drawFrameAxes(frame, matrix_coefficients, distortion_coefficients, rvec, tvec, marker_length*2)  
 
     return frame
 
@@ -52,6 +53,7 @@ if __name__ == '__main__':
     ap.add_argument("-k", "--K_Matrix", required=True, help="Path to calibration matrix (numpy file)")
     ap.add_argument("-d", "--D_Coeff", required=True, help="Path to distortion coefficients (numpy file)")
     ap.add_argument("-t", "--type", type=str, default="DICT_ARUCO_ORIGINAL", help="Type of ArUCo tag to detect")
+    ap.add_argument("-l", "--length", type=float, default=0.1, help="Length of the marker's side in meters")
     args = vars(ap.parse_args())
 
     
@@ -60,6 +62,7 @@ if __name__ == '__main__':
         sys.exit(0)
 
     aruco_dict_type = ARUCO_DICT[args["type"]]
+    marker_length = args["length"]
     calibration_matrix_path = args["K_Matrix"]
     distortion_coefficients_path = args["D_Coeff"]
     
@@ -75,7 +78,7 @@ if __name__ == '__main__':
         if not ret:
             break
         
-        output = pose_esitmation(frame, aruco_dict_type, k, d)
+        output = pose_esitmation(frame, aruco_dict_type, marker_length, k, d)
 
         cv2.imshow('Estimated Pose', output)
 
